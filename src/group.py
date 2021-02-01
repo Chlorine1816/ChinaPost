661ec9904f66f9adfcf32237471d20a56f7b1ac5
Chlorine <Chlorine1816@189.cn>
2021-02-01 15:56:52 +0800

    测试推送

--- src/group.py
+++ src/group.py
@@ -1,10 +1,21 @@
-import pandas as pd
-import os
+# -*- coding: utf8 -*-
+import requests,datetime,pytz,os
 
-if __name__=='__main__':
-    path='./data/'
-    filename='1.xlsx'
-    key=os.environ['KEY']
-    excel=pd.read_excel(path+filename)
-    print(excel.head())
-    print(key)
+# 微信推送
+def pushWechat(desp,nowtime):
+    params = {
+        #消息名称
+        'text': '当前时间',
+        #消息内容 
+        'desp': desp        
+        }
+    requests.post(scurl,params=params)
+
+if __name__ == "__main__":
+    SCKEY = os.environ['PUSHSCKEY']
+    #SERVER酱微信推送url
+    scurl = f"https://sc.ftqq.com/{SCKEY}.send"
+    tz = pytz.timezone('Asia/Shanghai')
+    nowtime = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+    desp='测试成功！'+ nowtime
+    pushWechat(desp,nowtime)
