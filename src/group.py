b27cbae5a4b18a04aa700d14d142b28582bc6ff9
Chlorine <Chlorine1816@189.cn>
2021-02-01 15:37:06 +0800

    更新

--- src/group.py
+++ src/group.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import numpy as np
+import os
 
 if __name__=='__main__':
     path='./data/'
     filename='1.xlsx'
+    key=os.environ['KEY']
     excel=pd.read_excel(path+filename)
     print(excel.head())
-    excel.to_excel(path+'2.xlsx')
+    print(key)
